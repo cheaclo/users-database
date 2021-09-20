606d18e988ab2c4dfeffaa612f87dc01b78691e3
Piotr Kalota <piotrkalota99@gmail.com>
2021-09-20 16:53:06 +0200

    Add AccountInfo entity

---
+++ src/main/java/com/cheaclo/userdatabase/entity/AccountInfo.java
@@ -0,0 +1,37 @@
+package com.cheaclo.userdatabase.entity;
+
+import lombok.*;
+import model.Gender;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class AccountInfo {
+    @Id
+    @SequenceGenerator(
+            name = "account_info_seq",
+            sequenceName = "account_info_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "account_info_seq"
+    )
+    @Column(name = "account_info_id")
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Date birthdayDate;
+    private Gender gender;
+    private String phoneNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+}
