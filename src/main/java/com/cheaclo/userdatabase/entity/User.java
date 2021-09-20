c26472b69235d243448532d128bb6ee9a87f9312
Piotr Kalota <piotrkalota99@gmail.com>
2021-09-20 16:58:32 +0200

    Add User entity

---
+++ src/main/java/com/cheaclo/userdatabase/entity/User.java
@@ -0,0 +1,29 @@
+package com.cheaclo.userdatabase.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @SequenceGenerator(
+            name = "user_seq",
+            sequenceName = "user_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_seq"
+    )
+    @Column(name = "user_id")
+    private Long id;
+    private Date createDate;
+    @ManyToOne
+    @JoinColumn(name = "account_info_id")
+    private AccountInfo accountInfo;
+}
