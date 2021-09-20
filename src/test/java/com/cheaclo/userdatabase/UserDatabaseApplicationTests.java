7c8c537e035b89d16f079bd74a13cbec78bb43a3
Piotr Kalota <piotrkalota99@gmail.com>
2021-09-20 16:23:13 +0200

    Init user database

---
+++ src/test/java/com/cheaclo/userdatabase/UserDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheaclo.userdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
