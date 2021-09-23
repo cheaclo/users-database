154973e8d078598ceda2242bf7fe96b3f973c1a4
Piotr Kalota <piotrkalota99@gmail.com>
2021-09-23 15:34:59 +0200

    Change mapping to delete one

--- src/main/java/com/cheaclo/userdatabase/controller/DeleteUserController.java
+++ src/main/java/com/cheaclo/userdatabase/controller/DeleteUserController.java
@@ -2,16 +2,12 @@ package com.cheaclo.userdatabase.controller;
 
 
 import com.cheaclo.userdatabase.entity.User;
-import com.cheaclo.userdatabase.model.request.AddUserRequestBody;
 import com.cheaclo.userdatabase.model.request.DeleteUserRequestBody;
 import com.cheaclo.userdatabase.repository.UserRepository;
-import com.cheaclo.userdatabase.service.AddUserParser;
-import com.cheaclo.userdatabase.service.CountryValidator;
-import com.cheaclo.userdatabase.service.response.AddUserResponse;
 import com.cheaclo.userdatabase.service.response.DeleteUserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +21,7 @@ public class DeleteUserController {
     private final UserRepository userRepository;
     private final DeleteUserResponse deleteUserResponse;
 
-    @PostMapping("/delete")
+    @DeleteMapping("/delete")
     public ResponseEntity<DeleteUserResponse> deleteUser(@Valid @RequestBody DeleteUserRequestBody request) {
         User user = userRepository.findFirstByIdAndAccountInfo_EmailIgnoreCase(request.getUserId(), request.getEmail());
         if (user == null)
